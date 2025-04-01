63671c50cdfa4cf2d28c9acc9ad7f3a9259ee72a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-01 17:43:54 -0500

    Add ICourierService

---
+++ CourierTracking.Application/Interfaces/ICourierService.cs
@@ -0,0 +1,12 @@
+﻿using CourierTracking.Domain.Entities;
+
+namespace CourierTracking.Application.Interfaces
+{
+    public interface ICourierService
+    {
+        Task<IEnumerable<Courier>> GetAllCouriersAsync();
+        Task<Courier> GetCourierByIdAsync(int id);
+        Task UpdateCourierLocationAsync(int courierId, double latitude, double longitude, double? speed);
+        Task<IEnumerable<LocationHistory>> GetCourierLocationHistoryAsync(int courierId);
+    }
+}
