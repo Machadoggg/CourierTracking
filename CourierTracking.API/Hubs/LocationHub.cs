615e32b1c29251d7c4c32db237c876d5c2c35d06
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-01 17:56:59 -0500

    Add LocationHub in API

---
+++ CourierTracking.API/Hubs/LocationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CourierTracking.API.Hubs
+{
+    public class LocationHub : Hub
+    {
+        public async Task SendLocationUpdate(int courierId, double latitude, double longitude, double? speed)
+        {
+            await Clients.All.SendAsync("ReceiveLocationUpdate", courierId, latitude, longitude, speed);
+        }
+    }
+}
