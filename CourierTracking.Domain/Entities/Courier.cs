d82d6a1605db9f52ac065f9876c5f3db9760be3b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-01 17:35:59 -0500

    Add Courier entity

---
+++ CourierTracking.Domain/Entities/Courier.cs
@@ -0,0 +1,11 @@
+﻿namespace CourierTracking.Domain.Entities
+{
+    public class Courier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Phone { get; set; } = default!;
+        public string VehicleType { get; set; } = default!;
+        public bool IsActive { get; set; } = true;
+    }
+}
