3bffe0651073ff57c4c8531ae3c7d300ac6b2b15
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-01 19:07:32 -0500

    Fix ubication CourierService

--- CourierTracking.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CourierTracking.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
--- CourierTracking.Infrastructure/Repositories/Repository.cs
+++ CourierTracking.Infrastructure/Repositories/Repository.cs
@@ -23,45 +23,6 @@ namespace CourierTracking.Infrastructure.Repositories
 
     
 
-    // Services/CourierService.cs
-    public class CourierService : ICourierService
-    {
-        private readonly IRepository<Courier> _courierRepository;
-        private readonly IRepository<LocationHistory> _locationRepository;
-
-        public CourierService(
-            IRepository<Courier> courierRepository,
-            IRepository<LocationHistory> locationRepository)
-        {
-            _courierRepository = courierRepository;
-            _locationRepository = locationRepository;
-        }
-
-        public async Task<IEnumerable<Courier>> GetAllCouriersAsync() =>
-            await _courierRepository.GetAllAsync();
-
-        public async Task<Courier> GetCourierByIdAsync(int id) =>
-            await _courierRepository.GetByIdAsync(id);
-
-        public async Task UpdateCourierLocationAsync(int courierId, double latitude, double longitude, double? speed)
-        {
-            var location = new LocationHistory
-            {
-                CourierId = courierId,
-                Latitude = latitude,
-                Longitude = longitude,
-                Speed = speed,
-                Timestamp = DateTime.UtcNow
-            };
-
-            await _locationRepository.AddAsync(location);
-        }
-
-        public async Task<IEnumerable<LocationHistory>> GetCourierLocationHistoryAsync(int courierId)
-        {
-            var locations = await _locationRepository.GetAllAsync();
-            return locations.Where(l => l.CourierId == courierId).OrderByDescending(l => l.Timestamp);
-        }
-    }
+    
 }
 
