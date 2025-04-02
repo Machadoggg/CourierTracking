a59b8e468c0c7790fb2613d59d98225c2748e6d7
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-04-02 12:56:02 -0500

    Add leaflet.js for Web project

--- CourierTracking.Web/Program.cs
+++ CourierTracking.Web/Program.cs
@@ -6,6 +6,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:7287/") // Reemplaza con tu URL de API
+    });
+
+//builder.Services.AddGoogleMaps();
 
 await builder.Build().RunAsync();
